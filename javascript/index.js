f89cdbf034994c7736eda393f7b202ef60d5f8d8
Alexey Sumachev <Dark--Templar@yandex.ru>
2021-10-29 17:12:26 +0300

    JS refactoring, Add READme

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,32 @@
-document.querySelector('.profile__edit-button').addEventListener('click',(event) => {
-  document.querySelector('.overlay').classList.add('overlay_open')
+const editButton = document.querySelector('.profile__edit-button')
+const nameEl = document.querySelector('.profile__name')
+const descEl = document.querySelector('.profile__desc')
+
+const overlay = document.querySelector('.overlay')
+const closeButton = document.querySelector('.overlay__close-button')
+const formElement = document.querySelector('.overlay__edit-form')
+const overlayInputName = document.querySelector('.overlay__input_name')
+const overlayInputDesc = document.querySelector('.overlay__input_desc')
+
+editButton.addEventListener('click',(event) => {
+  overlay.classList.add('overlay_open')
 })
 
-document.querySelector('.overlay__close-button').addEventListener('click',(event) => {
-  document.querySelector('.overlay').classList.remove('overlay_open')
+closeButton.addEventListener('click',(event) => {
+  overlay.classList.remove('overlay_open')
 })
 
 document.addEventListener('DOMContentLoaded',() => {
-  const nameEl = document.querySelector('.profile__name')
   const nameText = nameEl.textContent
-  document.querySelector('.overlay__input_name').value = nameText
+  overlayInputName.value = nameText
 
-  const descEl = document.querySelector('.profile__desc')
   const descText = descEl.textContent
-  document.querySelector('.overlay__input_desc').value = descText
-
+  overlayInputDesc.value = descText
 })
 
-const formElement = document.querySelector('.overlay__edit-form')
 formElement.addEventListener('submit',(event) => {
   event.preventDefault()
-  const nameEl = document.querySelector('.profile__name')
-  const descEl = document.querySelector('.profile__desc')
   nameEl.textContent = event.target.elements.name.value
   descEl.textContent = event.target.elements.desc.value
-  document.querySelector('.overlay').classList.remove('overlay_open')
+  overlay.classList.remove('overlay_open')
 })
